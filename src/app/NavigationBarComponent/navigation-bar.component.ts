ce63167827d40cdf8e57462fcd3d70966df27085
johnyno <johnyno@gmail.com>
2016-11-09 10:11:28 +0200

    header component added

--- src/app/NavigationBarComponent/navigation-bar.component.ts
+++ src/app/NavigationBarComponent/navigation-bar.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'navigation-bar',
-    templateUrl: 'navigationBar.component.html'
+    templateUrl: 'navigation-bar.component.html',
+    styleUrls:['navigation-bar.component.css']
 })
 
 export class NavigationBarComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './AppComponent/app.component';
 import {NavigationBarComponent} from "./NavigationBarComponent/navigation-bar.component";
+import {HeaderComponent} from "./HeaderComponent/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
