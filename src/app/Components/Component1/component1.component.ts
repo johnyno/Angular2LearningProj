ab5ee84cf2d1e5e96f73f189a2b2c90f29107ec8
johnyno <johnyno@gmail.com>
2016-11-13 16:59:26 +0200

    Routing: first steps. Two components.

---
+++ src/app/Components/Component1/component1.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'component1.component.html'
+})
+export class Component1 {
+  title = 'component 1';
+}
---
+++ src/app/Components/Component2/component2.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'component2.component.html'
+})
+export class Component2 {
+  title = 'component 2';
+}
