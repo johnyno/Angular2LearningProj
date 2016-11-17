f4229824389c9b61637d75c548f16db91b55c62b
johnyno <johnyno@gmail.com>
2016-11-17 08:49:50 +0200

    ToInit state

--- src/app/Components/Component1/component1.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'component-1',
-  templateUrl: 'component1.component.html'
-})
-export class Component1 {
-  title = 'component 1';
-}
